b469ff893091303af9f92eaa312e70002badb371
zhangjie <jie.zhang@csu.edu.cn>
2016-07-20 21:08:47 +0800

    add linux 环境下出错时记录函数调用堆栈信息

--- stack_dumper/stack_dumper.cpp
+++ stack_dumper/stack_dumper.cpp
@@ -1,6 +1,11 @@
-﻿#include "stack_dumper.h"
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "stack_dumper.h"
+
 
 StackDumper::StackDumper() {
+    stack_info_str_stream_.str("");
 #ifdef _WIN32
     enum { MAX_NAME_LENGTH = 256 };  // max length of symbols' name.
     // Initialize PSYMBOL_INFO structure.  
@@ -21,13 +26,12 @@ StackDumper::StackDumper() {
     stackframe_.AddrStack.Mode = AddrModeFlat;
     stackframe_.AddrFrame.Offset = context_.Ebp;
     stackframe_.AddrFrame.Mode = AddrModeFlat;
-    stack_info_str_stream_.str("");
     process_ = GetCurrentProcess();  // Get current process & thread.  
     thread_ = GetCurrentThread();
     // Initialize dbghelp library.  
     if (!SymInitialize(process_, NULL, TRUE)) {
         stack_info_str_stream_ << "Initialize dbghelp library ERROR!\n";
-	}
+    }
 #endif  // _WIN32
 }
 
@@ -36,15 +40,17 @@ StackDumper::~StackDumper() {
 }
 
 void StackDumper::Destory() {
+#ifdef _WIN32
     SymCleanup(process_);  // Clean up and exit.  
     free(symbol_);
     stack_info_str_stream_ << "StackDumper is cleaned up!\n";
+#endif  // _WIN32
 }
 
 std::string StackDumper::DumpStack() {
 #ifdef _WIN32
     stack_info_str_stream_ << "Call stack: \n";
-	// Enumerate call stack frame.  
+    // Enumerate call stack frame.  
     while (StackWalk64(IMAGE_FILE_MACHINE_I386, process_, thread_, &stackframe_,
             &context_, NULL, SymFunctionTableAccess64, SymGetModuleBase64, NULL)) {
         if (stackframe_.AddrFrame.Offset == 0) {  // End reaches.  
@@ -54,15 +60,88 @@ std::string StackDumper::DumpStack() {
             stack_info_str_stream_ << " ==> " << symbol_->Name << "\n";
         }
         if (SymGetLineFromAddr64(process_, stackframe_.AddrPC.Offset, &displacement_, &source_info_)) {
-			// Get source information.  
+            // Get source information.  
             stack_info_str_stream_ << "\t[" << source_info_.FileName << ":" << source_info_.LineNumber << "]\n";
-		}
-		else {
-			if (GetLastError() == 0x1E7) {  // If err_code == 0x1e7, no symbol was found.  
+        }
+        else {
+            if (GetLastError() == 0x1E7) {  // If err_code == 0x1e7, no symbol was found.  
                 stack_info_str_stream_ << "\tNo debug symbol loaded for this function.\n";
-			}
-		}
-	}
+            }
+        }
+    }
 #endif  // _WIN32
+
+#ifdef __linux__
+    Record_trace();
+#endif  // __linux__
     return stack_info_str_stream_.str();
 }
+
+
+
+#ifdef __linux__
+int32 StackDumper::Exec_cmd(const char* cmd) {
+    FILE *pp = popen(cmd, "r");  // 建立管道
+    if (!pp) {
+        stack_info_str_stream_ << "[Error] popen error in function StackDumper::Exec_cmd(const char* cmd)\n";
+        return -1;
+    }
+    char tmp[1024];
+    while (fgets(tmp, sizeof(tmp), pp) != NULL) {
+        if (tmp[strlen(tmp) - 1] == '\n') {
+            tmp[strlen(tmp) - 1] = '\0';  // 去除换行符
+        }
+        
+        stack_info_str_stream_ << "[" << tmp << "]\t";
+    }
+    stack_info_str_stream_ << "\n";
+    pclose(pp);  // 关闭管道
+
+    return 0;
+}
+
+void StackDumper::ParseName(char* str, char* exeName, char* addr) {
+    if (str == nullptr || exeName == nullptr || addr == nullptr) {
+        stack_info_str_stream_ << "[Error] nullptr in function StackDumper::ParseName(char* str, char* exeName, char* addr)\n";
+        return;
+    }
+    char* strTemp = str;
+    char* addrTemp;
+    while (*strTemp != '\0') {
+        if (*strTemp == '(')
+            memcpy(exeName, str, strTemp - str);
+
+        if (*strTemp == '[')
+            addrTemp = strTemp;
+
+        if (*strTemp == ']')
+            memcpy(addr, str + (addrTemp - str) + 1, strTemp - addrTemp - 1);
+        strTemp++;
+    }
+}
+
+void StackDumper::Record_trace()
+{
+    enum {MAX_CALLSTACK_DEPTH = 10};  // 需要打印堆栈的最大深度
+    void *array[MAX_CALLSTACK_DEPTH];
+
+    char **strings;
+
+    int32 depth = backtrace(array, MAX_CALLSTACK_DEPTH);
+    strings = backtrace_symbols(array, depth);
+
+    stack_info_str_stream_ << " ==> Obtained "<< depth << "stack frames.\n";
+
+    char cmd[1024] = {0}; 
+    char exeName[1024] = {0};
+    char addr[100] = {0};
+    for(int32 i = 0; i < depth; ++i) {
+      memset(cmd, 0, sizeof(cmd));
+      memset(exeName, 0, sizeof(exeName));
+      memset(addr, 0, sizeof(addr));
+      ParseName(strings[i], exeName, addr);
+      snprintf(cmd, sizeof(cmd)-1, "addr2line -f -e %s %s", exeName, addr);
+      Exec_cmd(cmd);
+    }     
+}
+#endif  // __linux__
--- stack_dumper/stack_dumper.h
+++ stack_dumper/stack_dumper.h
@@ -1,13 +1,16 @@
 ﻿#ifndef STACK_DUMPER_H
 #define STACK_DUMPER_H
 
+#include <sstream>
+
 #ifdef _WIN32
 #include <windows.h>  
 #include <dbghelp.h>
-#include <string>
-#include <sstream>
 #pragma comment (lib, "dbghelp.lib")  
+#else
+#include <execinfo.h>
 #endif  // _WIN32
+
 
 
 class StackDumper {
@@ -20,15 +23,21 @@ class StackDumper {
 
     std::string DumpStack();
 
+#ifdef __linux__
+    int32 Exec_cmd(const char* cmd);
+    void ParseName(char* str, char* exeName, char* addr);
+    void Record_trace();
+#endif  // __linux__
+
  private:
 #ifdef _WIN32
     UINT max_name_length_;              // Max length of symbols' name.
-    CONTEXT context_;				    // Store register addresses.
+    CONTEXT context_;                   // Store register addresses.
     STACKFRAME64 stackframe_;           // Call stack.
     HANDLE process_, thread_;           // Handle to current process & thread.
     PSYMBOL_INFO symbol_;               // Debugging symbol's information.
     IMAGEHLP_LINE64 source_info_;       // Source information (file name & line number)
-    DWORD displacement_;				// Source line displacement.
+    DWORD displacement_;                // Source line displacement.
 #endif  // _WIN32
     std::ostringstream stack_info_str_stream_;
 };
